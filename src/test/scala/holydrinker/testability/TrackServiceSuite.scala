9aee313295e8c40535aaad5032e7d0d6160760f3
Peppo <giuseppelorusso92@gmail.com>
2021-06-11 06:51:00 +0200

    add one test

--- src/test/scala/holydrinker/testability/TrackServiceSuite.scala
+++ src/test/scala/holydrinker/testability/TrackServiceSuite.scala
@@ -6,7 +6,7 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class TrackServiceSuite extends AnyFunSuite {
 
-  test("track service works fine") {
+  test("track service works fine for long song") {
 
     val events = Seq(
       ListenEvent(1000, 0, 100)
@@ -17,6 +17,19 @@ class TrackServiceSuite extends AnyFunSuite {
     val expected = Seq(
       Track(1000, "best of you", "foo fighters")
     )
+
+    assert(actual == expected)
+  }
+
+  test("track service works fine for short song") {
+
+    val events = Seq(
+      ListenEvent(1000, 0, 5)
+    )
+
+    val actual = TrackService.rebuildLongTrack(events, minSeconds = 10)
+
+    val expected = Seq.empty[Track]
 
     assert(actual == expected)
   }
